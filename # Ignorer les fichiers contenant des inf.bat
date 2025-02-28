3129fc2aaeb250d5832ee3ce99b280a14a798671
JamesTools007 <phoenixmodzcompany@gmail.com>
2025-02-28 14:42:30 +0100

    Add files via upload

---
+++ # Ignorer les fichiers contenant des inf.bat
@@ -0,0 +1,16 @@
+# Ignorer les fichiers contenant des informations sensibles
+.env
+
+# Ignorer les fichiers temporaires
+__pycache__/
+*.pyc
+*.pyo
+
+# Ignorer les logs et caches
+logs/
+*.log
+debug.log
+
+# Ignorer les fichiers de l'IDE (VS Code, PyCharm, etc.)
+.vscode/
+.idea/
